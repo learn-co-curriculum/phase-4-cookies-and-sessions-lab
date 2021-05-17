93bdeafbd93b65ddebee273d21a3371f93136c5e
ihollander <ianholla@gmail.com>
2021-05-17 07:11:29 -0400

    Added solution

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,15 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:pageviews_remaining] ||= 3
+    session[:pageviews_remaining] -= 1
+    
+    if session[:pageviews_remaining] > 0
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: { error: "Maximum pageview limit reached" }, status: :unauthorized
+    end
   end
 
   private
