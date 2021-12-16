143a10f386ded0c755602faa366f8b41428f2449
ihollander <ianholla@gmail.com>
2021-12-16 10:25:47 -0500

    Set payway after three pageviews

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
     session[:page_views] ||= 0
     session[:page_views] += 1
     
-    if session[:page_views] < 3
+    if session[:page_views] <= 3
       article = Article.find(params[:id])
       render json: article
     else
